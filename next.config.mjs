54e4373f874f14b7b50c1b8227829e04dc215a5b
Mitch Malinin <mitchmalinin@gmail.com>
2024-09-07 23:11:24 -0400

    added shadcn

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,9 @@
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -7,6 +13,7 @@ const nextConfig = {
   },
   webpack(config) {
     config.experiments = { ...config.experiments, topLevelAwait: true }
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src')
     return config
   },
 }
